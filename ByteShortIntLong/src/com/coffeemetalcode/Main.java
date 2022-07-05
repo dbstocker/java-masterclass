a905b417f2a485416484275c9bfbae5c00e64f33
David B. Stocker <coffeemetalcode@gmail.com>
2022-07-05 14:19:00 -0400

    through lecture 26

---
+++ ByteShortIntLong/src/com/coffeemetalcode/Main.java
@@ -0,0 +1,32 @@
+package com.coffeemetalcode;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /* byte - 8 bits */
+        byte minByteVal = Byte.MIN_VALUE;
+        byte maxByteVal = Byte.MAX_VALUE;
+        System.out.println("Byte Min Value = " + minByteVal);
+        System.out.println("Byte Max Value = " + maxByteVal);
+
+        /* short - 16 bits */
+        short minShortVal = Short.MIN_VALUE;
+        short maxShortVal = Short.MAX_VALUE;
+        System.out.println("Short Min Value = " + minShortVal);
+        System.out.println("Short Max Value = " + maxShortVal);
+
+        /* integer - 32 bits */
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Min Value = " + myMinIntValue);
+        System.out.println("Integer Max Value = " + myMaxIntValue);
+
+        /* long - 64 bits */
+        long minLongVal = Long.MIN_VALUE;
+        long maxLongVal = Long.MAX_VALUE;
+        System.out.println("Long Min Value = " + minLongVal);
+        System.out.println("Long Max Value = " + maxLongVal);
+
+    }
+}
---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello David!");
+
+        int firstNum = 15;
+        int secondNum = 12;
+        int thirdNum;
+
+        thirdNum = firstNum + secondNum;
+
+        int lastOne = 1000 - thirdNum;
+
+        System.out.println(thirdNum);
+        System.out.println(lastOne);
+    }
+}
