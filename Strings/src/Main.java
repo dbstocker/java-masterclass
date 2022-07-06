fff73b85c976db08c52e301e01f46e548b253059
David B. Stocker <coffeemetalcode@gmail.com>
2022-07-06 16:15:22 -0400

    complete through lesson 32

---
+++ Strings/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        /* String */
+        String stringVal1 = "The first string";
+
+        System.out.println("stringVal1 is equal to " + "\"" + stringVal1 + "\"");
+    }
+}
